0d00782cf1c0185be64841936cb664dbcd0a53d7
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-04-01 21:40:52 +0600

    level one handle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,31 @@ public class Main {
         Dragon dragon = new Dragon(2000,120,150,0);
         Hero hero = new Hero(1000,100,120,250,150);
 
+        int turn = 1;
+
+        //--------------------------level 1 ----------------------------------------
+
+        boolean heroAttack = (Math.random() < 0.7);
+        int damage = hero.getStrength() + hero.getWeapon() - dragon.getDefence();
+            if (heroAttack) {
+                dragon.setHealthPoint(dragon.getHealthPoint() - damage);
+                if (dragon.isAlive()) {
+                    System.out.println("Hero attacked Dragon and cause " + damage + " damage." +
+                            "Dragon has left " + dragon.getHealthPoint() + " health points");
+                } else {
+                    System.out.println(("Hero attacked and won!"));
+                    return;
+                }
+            } else {
+                System.out.println("Hero missed attack");
+                return;
+            }
+
+
+
+
+
+
 
 
     }
