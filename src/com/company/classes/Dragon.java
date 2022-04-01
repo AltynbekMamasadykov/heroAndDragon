f64480bb028b4158aba61d8dbbdd43d8a17429b7
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-04-01 18:38:01 +0600

    Initial commit

---
+++ src/com/company/classes/Dragon.java
@@ -0,0 +1,47 @@
+package com.company.classes;
+
+public class Dragon {
+    private int healthPoint;
+    private int defence;
+    private int strength;
+    private int weapon;
+
+    public Dragon(int healthPoint, int defence, int strength, int weapon) {
+        this.healthPoint = healthPoint;
+        this.defence = defence;
+        this.strength = strength;
+        this.weapon = weapon;
+    }
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
+    public void setHealthPoint(int healthPoint) {
+        this.healthPoint = healthPoint;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(int weapon) {
+        this.weapon = weapon;
+    }
+}
---
+++ src/com/company/classes/Hero.java
@@ -0,0 +1,57 @@
+package com.company.classes;
+
+public class Hero {
+    private int healthPoint;
+    private int defence;
+    private int strength;
+    private int weapon;
+    private int shield;
+
+    public Hero(int healthPoint, int defence, int strength, int weapon, int shield) {
+        this.healthPoint = healthPoint;
+        this.defence = defence;
+        this.strength = strength;
+        this.weapon = weapon;
+        this.shield = shield;
+    }
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
+    public void setHealthPoint(int healthPoint) {
+        this.healthPoint = healthPoint;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(int weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+
+    public void setShield(int shield) {
+        this.shield = shield;
+    }
+}
